55285f4aa9296fa1a79bdb24abce7c219c39a460
frontendgod <3054750147@qq.com>
2020-01-28 12:34:56 +0800

    p03 done

--- src/Pages/List.js
+++ src/Pages/List.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+
 class List extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() { 
-        return ( <div>List</div> );
+        return ( <div>{this.state.id}</div> );
+    }
+    componentDidMount(){
+        console.log(this.props)
+        let tempId = this.props.match.params.id
+        this.setState({
+            id: tempId
+        })
     }
 }
  
