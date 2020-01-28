5618e81066670afb9d45d76eb2287c5471223491
frontendgod <3054750147@qq.com>
2020-01-28 12:56:20 +0800

    p04 done

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            list: [
+                {cid: 123, title: '技术胖的个人博客-1'},
+                {cid: 456, title: '技术胖的个人博客-2'},
+                {cid: 789, title: '技术胖的个人博客-3'},
+            ]
+        }
     }
     render() { 
-        return ( <div>Index</div> );
+        return ( 
+            <div>
+                <h2>朱莉</h2>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <Link to={`/list/${item.cid}`}>{item.title}</Link>
+                                </li>
+                            )
+                        })
+                    }
+                </ul> 
+            </div>
+        );
     }
 }
  
